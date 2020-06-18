908b7b2a8cd2a3709337a0712aa93e9501a4b457
wwrk22 <wwrk22@gmail.com>
2020-06-18 15:16:41 -0400

    Added tetrominoes

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /* Tetrominoes */
+    const lTetrominoA = [
+        [0, width, width + 1, width + 2],
+        [1, 2, width + 1, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2, width * 2 + 1]
+    ]
+
+    const lTetrominoB = [
+        [2, width, width + 1, width + 2],
+        [1, width + 1, width * 2 + 1, width * 2 + 2],
+        [width, width + 1, width + 2, width * 2],
+        [0, 1, width + 1, width * 2 + 1]
+    ]
+
+    const zTetrominoA = [
+        [0, 1, width + 1, width + 2],
+        [2, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width * 2 + 1, width * 2 + 2],
+        [1, width, width + 1, width * 2]
+    ]
+
+    const zTetrominoB = [
+        [1, 2, width, width + 1],
+        [1, width + 1, width + 2, width * 2 + 2],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1]
+    ]
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const iTetromino = [
+        [width, width + 1, width + 2, width + 3],
+        [2, width + 2, width * 2 + 2, width * 3 + 2],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1]
+    ]
+})
