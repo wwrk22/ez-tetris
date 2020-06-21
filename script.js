dabd1690ef7c246f03a8619c6aeaaf6c81340749
Won Rhim <wwrk22@gmail.com>
2020-06-20 21:38:20 -0400

    Tetrominoes now freeze at bottom and stack

--- script.js
+++ script.js
@@ -84,36 +84,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     const gameInfo = {
+        gameStarted: false,
         scoreDisplay: document.querySelector('#score-display'),
         startBtn: document.querySelector('#start-btn'),
         score: 0,
-        /* Used for choosing next-up tetromino randomly. */
-        nextUpRandom: 0,
         /* Used to move a tetromino every one second. */
         timer: undefined,
         /* Tetromino will be 'currentPosition' blocks to the right of the left wall of the board. */
         currentPosition: 4,
         /* Indicates the rotation the current tetromino is. */
-        currentRotation: 0
+        currentRotation: 0,
     }
 
-    
-    /* Randomly select a tetromino in its first rotation.
-       Random is used to index a tetromino, and currentRotation is used to index
-       the first rotation of the randomly chosen tetromino. */
-    let random = Math.floor(Math.random() * tetrominoes.length)
-    let currentTetromino = tetrominoes[random][gameInfo.currentRotation]
+    /* Used for choosing next-up tetromino randomly. */
+    let nextUpRandomIndex = 0
+    /* Randomly select a tetromino in its first rotation. 'randomIndex' is used to index a tetromino,
+       and currentRotation is used to index the first rotation of the randomly chosen tetromino. */
+    let randomIndex = Math.floor(Math.random() * tetrominoes.length)
+    let currentTetromino = tetrominoes[randomIndex][gameInfo.currentRotation]
 
     /* Draws the randomly chosen tetromino in the rotation indexed
      * by currentRotation.
      */
     function draw() {
         /* Move iTetromino one block to the left, and leave the other tetrominoes as they are. */
-        if (random === 6) {
-            gameInfo.currentPosition--;
+        if (!gameInfo.gameStarted && randomIndex === 6) {
+            gameInfo.currentPosition--
+            gameInfo.gameStarted = true
         }
         currentTetromino.forEach(index => {
-            boardInfo.boardBlocks[gameInfo.currentPosition + index].style.backgroundColor = colors[random];
+            boardInfo.boardBlocks[gameInfo.currentPosition + index].style.backgroundColor = colors[randomIndex];
         })
     }
 
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
      */
     function undraw() {
         currentTetromino.forEach(index => {
-            boardInfo.boardBlocks[currentPosition + index].style.backgroundColor = ""
+            boardInfo.boardBlocks[gameInfo.currentPosition + index].style.backgroundColor = ""
         })
     }
 
@@ -143,4 +143,38 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     document.addEventListener('keyup', controls);
+
+    /*
+     * Moves the current tetromino down by one line.
+     */
+    function moveDown() {
+        undraw()
+        gameInfo.currentPosition += boardWidth
+        draw()
+        freeze()
+    }
+
+    /*
+     * If the row right below the tetromino is out-of-bounds, then 
+     * the tetromino is frozen in place.
+     */
+    function freeze() {
+        if (currentTetromino.some(
+            index => boardInfo.boardBlocks[gameInfo.currentPosition + index + boardWidth].classList.contains('occupied-block'))) {
+            /* Prevent other tetrominoes from using the blocks on which the current tetromino has been frozen. */
+            currentTetromino.forEach(
+                index => boardInfo.boardBlocks[gameInfo.currentPosition + index].classList.add('occupied-block'))
+            /* Generate new random tetromino */
+            randomIndex = nextUpRandomIndex
+            nextUpRandomIndex = Math.floor(Math.random() * tetrominoes.length)
+            currentTetromino = tetrominoes[randomIndex][0]
+            gameInfo.currentPosition = 4
+            draw()
+
+        }
+    }
+    
+    /* When game starts, draw() has to be called once first. */
+    draw()
+    gameInfo.timer = setInterval(moveDown, 1000)
 })
