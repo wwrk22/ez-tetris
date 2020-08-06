a94d471202921dd7aa131f1885369e8d8044ec12
wwrk22 <wwrk22@gmail.com>
2020-08-06 15:43:04 -0400

    Issue Fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 <?php
 
+    $plugin_dir_url = plugin_dir_url(__DIR__);
     $footer = "<!-- Allow page to load quicker by putting script-tag here -->";
-    $footer .= "<script src=\"assets/js/game-script.js\"></script>";
+    $footer .= "<script src=\"" . $plugin_dir_url . "assets/js/game-script.js\"></script>";
 
 ?>
