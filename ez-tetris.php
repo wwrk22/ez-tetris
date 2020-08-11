51b34abc3f57ed42204d1a1f3d275d1b4030b4e3
wwrk22 <wwrk22@gmail.com>
2020-08-11 18:48:13 -0400

    Not done yet

--- ez-tetris.php
+++ ez-tetris.php
@@ -1,54 +1,60 @@
 <?php
 
-    /*
-    Plugin Name: EZ-Tetris
-    Plugin URI: https://thewongalaxy.com
-    Description: Game of tetris that can be embedded with shortcode.
-    Version: 1.0
-    Author: Won Rhim
-    Author URI: https://thewongalaxy.com
-    */
-
-    include "includes/header.php";
-    include "includes/body.php";
-    include "includes/footer.php";
+/**
+ * Plugin Name:       EZ Tetris
+ * Plugin URI:        https://thewongalaxy.com
+ * Description:       Game of tetris with shortcode.
+ * Version:           1.0.0
+ * Author:            Won Rhim
+ * Author URI:        https://thewongalaxy.com
+ */
 
 
-    function wpet_enqueue_scripts() {
-
-        wp_enqueue_style('ez-tetris-style', plugin_dir_url(__FILE__) . 'assets/css/ez-tetris-style.css');
-        wp_enqueue_script('ez-tetris-script', plugin_dir_url(__FILE__) . 'assets/js/game-script.js');
-
-    }
-
-    add_action('wp_enqueue_scripts', 'wpet_enqueue_scripts');
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 
-    function generate_html() {
+include "includes/body.php";
+
+
+function et_add_scripts() {
+    // CSS
+    wp_enqueue_style('museo-moderno-font', 'https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap');
+    wp_enqueue_style('indie-flower-font', 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+    wp_enqueue_style('ez-tetris-style', plugins_url() . '/ez-tetris/assets/css/ez-tetris-style.css');
+
+    // JS
+    wp_enqueue_script('ez-tetris-script', plugins_url() . '/ez-tetris/assets/js/ez-tetris-main.js');
+}
+
+add_action('wp_enqueue_scripts', 'et_add_scripts');
+
+
+function generate_html() {
         
-        global $header;
-        global $body;
-        global $footer;
+    global $header;
+    global $body;
+    global $footer;
 
-        echo $header;
-        echo $body;
-        echo $footer;
+    echo $header;
+    echo $body;
+    echo $footer;
 
-    }
-
-
-    function cf_shortcode() {
-        
-        /* Turn on output buffering which we do via echo in generate_html() */
-        ob_start();
-        generate_html();
-
-        /* ob_get_clean() is equivalent to calling bot ob_get_contents() and ob_end_clean() */
-        return ob_get_clean();
-
-    }
+}
 
 
-    add_shortcode('ez_tetris', 'cf_shortcode');
+function cf_shortcode() {
+    
+    /* Turn on output buffering which we do via echo in generate_html() */
+    ob_start();
+    generate_html();
+
+    /* ob_get_clean() is equivalent to calling bot ob_get_contents() and ob_end_clean() */
+    return ob_get_clean();
+
+}
+
+add_shortcode('ez_tetris', 'cf_shortcode');
 
 ?>
--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $footer = "<!-- Allow page to load quicker by putting script-tag here -->";
-    $footer .= "<script src=\"" . plugin_dir_url(__DIR__) . "assets/js/game-script.js\"></script>";
-
-?>
