8efb956f6ec1b02cea849898865de7eabc0aa023
Won Rhim <wwrk22@gmail.com>
2020-08-11 21:15:09 -0400

    Update ez-tetris.php

--- ez-tetris.php
+++ ez-tetris.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Plugin Name:       EZ Tetris
- * Plugin URI:        https://thewongalaxy.com
- * Description:       Game of tetris with shortcode.
- * Version:           1.0.0
- * Author:            Won Rhim
- * Author URI:        https://thewongalaxy.com
- */
-
-
 if (!defined('ABSPATH')) {
     exit;
 }
