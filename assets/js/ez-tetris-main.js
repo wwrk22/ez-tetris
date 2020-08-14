35165a9acebc20838c950d4209af4743efe7d416
wwrk22 <wwrk22@gmail.com>
2020-08-13 20:56:34 -0400

    To-do done

--- assets/js/ez-tetris-main.js
+++ assets/js/ez-tetris-main.js
@@ -491,6 +491,7 @@ document.addEventListener('DOMContentLoaded', () => {
         * start/resume/pause the game.
         */
     gameInfo.startBtn.addEventListener('click', () => {
+
         if (gameInfo.timer) {
             
             /* Allow browser to respond to keys */
@@ -535,6 +536,9 @@ document.addEventListener('DOMContentLoaded', () => {
             pencil.draw()
             gameInfo.timer = setInterval(moveDown, 1000)
         }
+
+        /* Set focus off the button so pressing spacebar doesn't resume the game */
+        document.activeElement.blur()
     })
 
     const keyDownHandler = function (event) {
